ea77ba717e64402331df0067b4d0f8ec24d1e5f2
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-11-03 13:29:38 +0330

    i maked full screen slider with swiper.js

--- Front-End/content/js/swiper/script.js
+++ Front-End/content/js/swiper/script.js
@@ -1,21 +1,16 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-
-    // If we need pagination
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     pagination: {
-        el: '.swiper-pagination',
+        el: ".swiper-pagination",
+        clickable: true,
     },
-
-    // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 });
